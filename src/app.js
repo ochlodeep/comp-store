655042069605c4e3518af17e46fa02351637b049
Edward <edwardhan234@gmail.com>
2023-02-26 16:56:44 -0800

    create react

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
-const app = require('express')();
+import logo from './logo.svg';
+import './App.css';
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!\n');
-});
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-app.listen(3000, '0.0.0.0');
+export default App;
